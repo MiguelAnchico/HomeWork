74ce97b11175531a90a6c0b49f6545681b4a8d62
Miguel Angel <miguel.anchico@uao.edu.co>
2023-03-03 21:33:28 -0500

    Challenge Completed

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,29 +1,30 @@
 import { useState } from "react";
 
 export const FirstApp = () => {
-  const [count, setCount] = useState(0);
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
 
-  const handleAdd = () => {
-    setCount(count + 1);
-  };
-
-  const handleSubstract = () => {
-    setCount(count - 1);
-  };
-
-  const handleReset = () => {
-    setCount(0);
+  const addCategory = () => {
+    setCategories([...categories, category]);
+    setCategory("");
   };
 
   return (
     <div>
-      <h1>Counter</h1>
-      <span>{count}</span>
-      <br />
+      <h1>New Category</h1>
+      <input
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+        type="text"
+      />
+      <button onClick={addCategory}>Add Category</button>
 
-      <button onClick={handleSubstract}>Subtract</button>
-      <button onClick={handleReset}>Reset</button>
-      <button onClick={handleAdd}>Add</button>
+      <h2>Categories</h2>
+      <ol>
+        {categories?.map((category, index) => (
+          <li key={index}>{category}</li>
+        ))}
+      </ol>
     </div>
   );
 };
