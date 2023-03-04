546d602fd1b3b54783d12802fda1a7129dccf4de
Miguel Angel <miguel.anchico@uao.edu.co>
2023-03-03 21:24:21 -0500

    Challenge Completed

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
 export const FirstApp = () => {
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubstract = () => {
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <div>
-      <h1>FirstApp</h1>
-      <span>10</span>
+      <h1>Counter</h1>
+      <span>{count}</span>
+      <br />
+
+      <button onClick={handleSubstract}>Subtract</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleAdd}>Add</button>
     </div>
   );
 };
